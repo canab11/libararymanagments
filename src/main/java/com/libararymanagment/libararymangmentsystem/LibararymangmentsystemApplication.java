138546454925ac0d5fb8a4ad5d4ee22574762a2f
canab mukhtar <canabmukhtarcabdulle@gmail.com>
2026-07-08 03:59:43 +0300

    Initial commit

---
+++ src/main/java/com/libararymanagment/libararymangmentsystem/LibararymangmentsystemApplication.java
@@ -0,0 +1,13 @@
+package com.libararymanagment.libararymangmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibararymangmentsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibararymangmentsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libararymanagment/libararymangmentsystem/LibararymangmentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libararymanagment.libararymangmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibararymangmentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
